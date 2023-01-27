c02e390b38fce9e29726d72488f4c7eea0fabf36
putusujane <blogibagi@gmail.com>
2023-01-27 15:10:38 +0700

    sampai filesystem (fs)

--- nodejs-basic/index.js
+++ nodejs-basic/index.js
@@ -11,3 +11,30 @@
 
 // const cpuInformation = process.memoryUsage();
 // console.log(cpuInformation);
+
+// Node Package Manager
+// const moment = require("moment");
+
+// const date = moment().format("DD MM YYYY");
+
+// console.log(date);
+
+// Filesystem (fs)
+// const fs = require("fs");
+
+// const fileReadCallback = (error, data) => {
+//     if(error) {
+//         console.log("Fail to read file");
+//         return;
+//     }
+
+//     console.log(data);
+// }
+
+// fs.readFile("todo.txt", "utf-8", fileReadCallback); // Asynchronous
+
+// Synchronous
+// const fs = require("fs");
+// const data = fs.readFileSync("todo.txt", "utf-8");
+// console.log(data);
+
